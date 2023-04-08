a574566ef51aa09b4e8109cfe5591c1faac976c5
monvanCe <omerfkoca@gmail.com>
2023-04-09 02:29:38 +0300

    transferred to homepage

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,21 +1,266 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class Movie {
+  final int id;
+  final String title;
+  final String posterPath;
+
+  Movie({required this.id, required this.title, required this.posterPath});
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      posterPath: json['poster_path'],
+    );
+  }
+}
+
+class MovieList {
+  final List<Movie> movies;
+
+  MovieList({required this.movies});
+
+  factory MovieList.fromJson(Map<String, dynamic> json) {
+    final List<dynamic> results = json['results'];
+    final List<Movie> movies =
+        results.map((result) => Movie.fromJson(result)).toList();
+    return MovieList(movies: movies);
+  }
+}
+
+class MoviesList extends StatefulWidget {
+  final List<Movie> movies;
+
+  const MoviesList({super.key, required this.movies});
+
+  @override
+  _MoviesListState createState() => _MoviesListState();
+}
+
+class _MoviesListState extends State<MoviesList> {
+  final _scrollController = ScrollController();
+  bool _showLeftButton = false;
+  bool _showRightButton = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(() {
+      setState(() {
+        _showLeftButton = _scrollController.offset >= 256;
+        _showRightButton = _scrollController.position.pixels <
+            _scrollController.position.maxScrollExtent;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'Now Playing',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.2,
+          child: Stack(
+            children: [
+              ListView.builder(
+                controller: _scrollController,
+                scrollDirection: Axis.horizontal,
+                itemCount: widget.movies.length,
+                itemBuilder: (context, index) {
+                  final movie = widget.movies[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        'https://image.tmdb.org/t/p/w185${movie.posterPath}',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+              Positioned(
+                top: 0,
+                bottom: 0,
+                left: 0,
+                child: Visibility(
+                  visible: _showLeftButton,
+                  child: SizedBox(
+                    width: 40.0,
+                    height: 40.0,
+                    child: ElevatedButton(
+                      onPressed: () => _scrollController.animateTo(
+                        0,
+                        curve: Curves.easeInOut,
+                        duration: const Duration(milliseconds: 500),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black.withOpacity(0.5),
+                        shape: const CircleBorder(),
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Icon(Icons.arrow_back),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                  top: 0,
+                  bottom: 0,
+                  right: 0,
+                  child: Visibility(
+                    visible: _showRightButton,
+                    child: SizedBox(
+                      width: 40.0,
+                      height: 40.0,
+                      child: ElevatedButton(
+                        onPressed: () => _scrollController.animateTo(
+                          _scrollController.offset + 256,
+                          curve: Curves.easeInOut,
+                          duration: const Duration(milliseconds: 500),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black.withOpacity(0.5),
+                          shape: const CircleBorder(),
+                          padding: EdgeInsets.zero,
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: const Text('Second Page'),
+        ),
+      ],
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<List<Movie>> fetchMovies() async {
+    const String apiKey = '4f098a761153500ca2a6a2d39980f694';
+    const String url =
+        'https://api.themoviedb.org/3/movie/now_playing?api_key=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    final decodedData = json.decode(response.body);
+    final MovieList movieList = MovieList.fromJson(decodedData);
+    final List<Movie> movies = movieList.movies;
+    return movies;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: Colors.black,
+        title: const Text('Emowl Welcome!'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.account_circle_outlined),
+          ),
+        ],
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Go to Second Page'),
-          onPressed: () {
-            Navigator.pushNamed(context, '/second');
-          },
+      drawer: Drawer(
+        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Emirhan Şahin"),
+              accountEmail: Text("emowl@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                child: Icon(Icons.person),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.home,
+                color: Colors.red,
+              ),
+              title: const Text('Home'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.settings,
+                color: Colors.green,
+              ),
+              title: const Text('Settings'),
+              onTap: () {},
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      extendBody: true,
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () {},
+            ),
+            const Text('Ana Sayfa'),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {},
+            ),
+            const Text('Ara')
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
+      body: FutureBuilder(
+        future: fetchMovies(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final List<Movie> movies = snapshot.data!;
+            return MoviesList(movies: movies);
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
